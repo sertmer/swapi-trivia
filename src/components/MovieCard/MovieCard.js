29ca805f9fec71281236132bfe93d0b301d58558
sertmer <scottertmer@gmail.com>
2019-12-03 15:17:02 -0700

    Reformat JSX in MovieCard

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,9 +3,12 @@ import './MovieCard.css'
 
 const MovieCard = ({ episode, title, releaseDate }) => {
   return (
-    <article>
-      <h2>Episode {episode}: {title}</h2>
-      <p>Released: {releaseDate}</p>
+    <article className='MovieCard'>
+      <h2>Episode {episode}:</h2>
+      <h3>{title}</h3>
+      <p>Released:</p>
+      <p>{releaseDate}</p>
+      <button>View Characters</button>
     </article>
   )
 }
