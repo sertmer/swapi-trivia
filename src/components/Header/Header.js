3c4fab054de14bed909a290367786dd53dd1d931
Roger Hughes <Rogerallenhughes@hotmail.com>
2019-12-03 15:17:09 -0700

    Add key values to header to append user Form submission

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
 
-const Header = () => {
+const Header = ({name, rank, quote}) => {
   return (
     <nav>
       <h1>Star Wars Trivia Database</h1>
+      <p>{rank} {name}</p>
+      <p>{quote}</p>
     </nav>
   )
 }
