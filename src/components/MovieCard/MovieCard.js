2c050c52958d897a7f2bfbb7f2a3608486174fca
Roger Hughes <Rogerallenhughes@hotmail.com>
2019-12-04 14:23:07 -0700

    Add images to coincide with their respective movies

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './MovieCard.css'
+import image1 from '../../images/1.jpg'
+import image2 from '../../images/2.jpg'
+import image3 from '../../images/3.jpg'
+import image4 from '../../images/4.jpg'
+import image5 from '../../images/5.jpg'
+import image6 from '../../images/6.jpeg'
+import image7 from '../../images/7.jpg'
 
+const images = [image1, image2, image3, image4, image5, image6, image7]
 const MovieCard = ({ episode, title, releaseDate }) => {
   return (
     <article className='MovieCard'>
       <div>
         <h2>Episode {episode}:</h2>
         <h3>{title}</h3>
+      </div>
+      <div>
+        <img src={images[episode - 1]} alt='movie cover art'/>
       </div>
       <div>
         <p>Released:</p>
