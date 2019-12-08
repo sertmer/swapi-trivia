085668786fee6e383b765846f0f484e1b68265b2
sertmer <scottertmer@gmail.com>
2019-12-08 13:06:20 -0700

    Re-implement Routes for all components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -44,56 +44,41 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <Header 
-          name={this.state.name}
-          rank={this.state.rank}
-          quote={this.state.quote}
-          signOut={this.signOut}
-        />
-        <LandingForm 
-          updateState={this.updateState}
+        <header>
+          <Route path='/' render={() => 
+            <Header 
+                name={this.state.name}
+                rank={this.state.rank}
+                quote={this.state.quote}
+                signOut={this.signOut}
+            />}
+          /> 
+        </header>
+        <main>
+          <Route exact path='/' render={() => 
+            <LandingForm 
+              updateState={this.updateState}
+            />}
           />
-        <MovieContainer 
-          className='MovieContainer'
-          movies={this.state.movies}
-          updateState={this.updateState}
+          <Route exact path='/movies'render={() => 
+            <MovieContainer 
+              className='MovieContainer'
+              movies={this.state.movies}
+              updateState={this.updateState}
+            />}
           />
-        {this.state.currentMovie === null ? null :
-        <CharacterContainer
-        characters={this.state.movies[this.state.currentMovie].characters}
-        />}
+          <Route path='/movies/:movie_id' render={({ match }) => {
+            console.log(match)
+            return (
+              <CharacterContainer
+                characters={this.state.movies[this.state.currentMovie].characters}
+              />
+            )} 
+          }/>
+        </main>
       </div>
     )
   }
 }
-//       <>
-//         <header>
-//         <Route path='/' render={() => 
-//             <Header
-//               name={this.state.name}
-//               rank={this.state.rank}
-//               quote={this.state.quote}
-//               signOut={this.signOut}
-//             />}
-//           />
-//         </header>
-//         <main className="App">
-//           <Route exact path='/' render={() => 
-//             <LandingForm 
-//             updateState={this.updateState} 
-//             />} 
-//           />
-//           <Route path='/movies' render={() =>
-//             <MovieContainer 
-//             className='MovieContainer'
-//             movies={this.state.movies}
-//           />}
-//           />
-  
-//         </main>
-//       </>
-//       )
-//     }
   
 export default App;
