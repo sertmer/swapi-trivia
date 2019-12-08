21375c78990cae35cc85816675ee73d57d66a9f2
sertmer <scottertmer@gmail.com>
2019-12-08 10:52:28 -0700

    Remove 'renders without crashing test'

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -10,12 +10,6 @@ describe('App', () => {
     wrapper = shallow(<App />)
   })
   
-  it('renders without crashing', () => {
-      const div = document.createElement('div')
-      ReactDOM.render(<App />, div)
-      ReactDOM.unmountComponentAtNode(div)
-  })
-
   it('should match the snapshot', () => {   
     expect(wrapper).toMatchSnapshot()
   })
