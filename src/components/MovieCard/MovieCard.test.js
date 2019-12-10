7017d834ecb1d9935af12b45bc348106b598b4fb
sertmer <scottertmer@gmail.com>
2019-12-09 20:34:50 -0700

    Add test for findCurrentMovie in MovieCard

--- src/components/MovieCard/MovieCard.test.js
+++ src/components/MovieCard/MovieCard.test.js
@@ -4,13 +4,26 @@ import { shallow } from 'enzyme'
 
 describe('MovieCard', () => {
   let wrapper
-
+  
+  let mockProps = {
+    episode: 1,
+    title: 'spaceballs',
+    releaseDate: '12-12-12',
+    updateState: jest.fn()
+  }
   beforeEach(() => {
-    wrapper = shallow(<MovieCard />)
+    wrapper = shallow(<MovieCard {...mockProps}/>)
   })
 
   it('should match the snapshot', () => {   
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('should update state when findCurrentMovie is called', () => {
+    const mockEvent = {target: {dataset: {
+      category: 1
+    }}}
+    wrapper.instance().findCurrentMovie(mockEvent)
+    expect(wrapper.state('currentMovie')).toEqual(1)
+  })
 })
