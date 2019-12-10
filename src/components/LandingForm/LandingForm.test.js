6c213b3278cb227b179703b49fddf6071438a0cc
sertmer <scottertmer@gmail.com>
2019-12-09 19:51:40 -0700

    Add tests to LandingForm

--- src/components/LandingForm/LandingForm.test.js
+++ src/components/LandingForm/LandingForm.test.js
@@ -6,11 +6,50 @@ describe('LandingForm', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = shallow(<LandingForm />)
+    wrapper = shallow(<LandingForm updateState={jest.fn()}/>)
   })
 
   it('should match the snapshot', () => {   
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('should update state when handleChange is invoked', () => {
+    const mockNameEvent = {target: {name: 'name', value: 'bob'}}
+    const mockQuoteEvent = {target: {name: 'quote', value: 'there is no try'}}
+    const mockRankEvent = {target: {name: 'rank', value: 'padawan'}}
+    const mockDefaultState = {
+      name: '',
+      quote: '',
+      rank: '',
+      isFinished: false
+    }
+
+    expect(wrapper.state()).toEqual(mockDefaultState)
+
+    wrapper.instance().handleChange(mockNameEvent)
+    expect(wrapper.state('name')).toEqual('bob')
+    expect(wrapper.state('isFinished')).toEqual(false)
+
+    wrapper.instance().handleChange(mockQuoteEvent)
+    expect(wrapper.state('quote')).toEqual('there is no try')
+
+    wrapper.instance().handleChange(mockRankEvent)
+    expect(wrapper.state('rank')).toEqual('padawan')
+    expect(wrapper.state('isFinished')).toEqual(true)
+  })
+
+  it.skip('should call submitInfo when the button is clicked', () => {
+    const mockFinishedState = {
+      name: 'bob',
+      quote: 'there is no try',
+      rank: 'Jedi Knight',
+      isFinished: true
+    }
+    
+    wrapper.setState(mockFinishedState)
+    wrapper.instance().submitInfo = jest.fn()
+    wrapper.find('.form_button').simulate('click')
+
+    expect(wrapper.instance().submitInfo).toHaveBeenCalled()
+  })
 })
