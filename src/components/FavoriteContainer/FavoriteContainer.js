55ab927d32efe6bccf7bfaa46966697d03f5cf7e
sertmer <scottertmer@gmail.com>
2019-12-09 21:21:49 -0700

    Adjust key to be the character's name, update snap

--- src/components/FavoriteContainer/FavoriteContainer.js
+++ src/components/FavoriteContainer/FavoriteContainer.js
@@ -8,7 +8,7 @@ const FavoriteContainer = ({ characters }) => {
     return (
         <CharacterCard  
         className='character_card'
-        key={(Date.now() + Math.random())}
+        key={character.name}
         name={character.name}
         homeworld={character.homeworld}
         population={character.population}
