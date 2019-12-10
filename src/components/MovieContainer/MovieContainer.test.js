0d4d29016c3be497dd64bd462ebb9a13f543ce66
sertmer <scottertmer@gmail.com>
2019-12-09 19:58:51 -0700

    Add second snapshot for loading path

--- src/components/MovieContainer/MovieContainer.test.js
+++ src/components/MovieContainer/MovieContainer.test.js
@@ -4,13 +4,22 @@ import { shallow } from 'enzyme'
 
 describe('MovieContainer', () => {
   let wrapper
+  let mockMovies = {
+    episode: 1,
+    title: 'spaceballs',
+    release_date: '12-12-12',
+  }
 
   beforeEach(() => {
-    wrapper = shallow(<MovieContainer movies={[]}/>)
+    wrapper = shallow(<MovieContainer movies={[mockMovies]}/>)
   })
 
   it('should match the snapshot', () => {   
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('should match the loading snapshot', () => {
+    wrapper = shallow(<MovieContainer movies={[]}/>)
+    expect(wrapper).toMatchSnapshot()
+  })
 })
